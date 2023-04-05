75071e7a2954d29d24998c247b7efc6ccd73d290
mloeff <michaelloeffler23@gmail.com>
2023-04-04 20:35:18 -0500

    creates file tree and adds some notes from the Challenge requirements page

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+// /api/users
+//----------------
+// get all users
+// get single user by id and bring in thought and friend data
+// post a new user
+  // example data
+  // {
+  //     "username": "lernantino",
+  //     "email": "lernantino@gmail.com"
+  // }
+// put to update a user by id
+// delete to remove a user by id
+
+
+// /api/users/:userId/friends/:friendId
+//----------------
+// post to add a new friend to a user's friend list
+// delete to remove a friend from a user's friend list
