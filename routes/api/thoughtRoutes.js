3020c5a8535b0d61931946fe8f719ae9d015a92e
mloeff <michaelloeffler23@gmail.com>
2023-04-04 22:22:25 -0500

    sets up userRoutes and thoughtRoutes: this way everything is ready to go once the functions referenced in the respective controller files are created

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,25 +1,27 @@
-//  /api/thoughts
-// -----------------
-// GET to get all thoughts
-
-// GET to get a single thought by its _id
-
-// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
    // // example data
-   // {
-   //   "thoughtText": "Here's a cool thought...",
-   //   "username": "lernantino",
-   //   "userId": "5edff358a0fcb779aa7b118b"
-   // }
+      // {
+      //   "thoughtText": "Here's a cool thought...",
+      //   "username": "lernantino",
+      //   "userId": "5edff358a0fcb779aa7b118b"
+      // }
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController');
 
-// PUT to update a thought by its _id
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
 
-// DELETE to remove a thought by its _id
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
 
-//  /api/thoughts/:thoughtId/reactions
-// -------------------------------------
-// POST to create a reaction stored in a single thought's reactions array field
-
-// DELETE to pull and remove a reaction by the reaction's reactionId value
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  // example data
+    // {
+    //   "username": "lernantino",
+    //   "email": "lernantino@gmail.com"
+    // }
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
