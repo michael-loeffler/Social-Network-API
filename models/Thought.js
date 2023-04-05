e3a6d57c643b81c0b6cddb0ba935f7ae65460e4b
mloeff <michaelloeffler23@gmail.com>
2023-04-04 21:52:52 -0500

    completes Thought model

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: [true, 'thoughtText is a required field'],
+            minlength: [1, 'thoughtText must be at least 1 character'],
+            maxlength: [280, 'thoughtText cannot be more than 280 characters'],
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: value => value.toDateString()
+        },
+        username: {
+            type: String,
+            required: [true, 'Username is a required field'],
+            trim: true, 
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
+
